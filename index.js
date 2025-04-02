d58fd8d04473099dd62b5b927b3d41e4a4300e50
Metellus <metellusjunior56@gmail.com>
2025-04-02 12:52:25 -0400

    Metellus complete lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats=[
+    "Milo",
+    "Otis",
+    "Garfield"
+]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop()
+}
+function destructivelyFirstLastCat(name){
+    cats.shift()
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift()
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
+function removeLastCat(){
+    return cats.slice(0, -1)
+}
+
+function appendCat(name){
+    const allCats = [...cats, name];
+    return allCats;
+}
+
+function prependCat(name){
+    const allCats =cats.slice()
+    allCats.unshift(name)
+    return allCats;
+}
